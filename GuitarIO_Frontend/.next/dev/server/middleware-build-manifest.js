da5111e1b98276045649813f87208e0f07db80da
yasineao <yassuke4@gmail.com>
2026-03-20 20:16:27 +0100

    css fix

--- GuitarIO_Frontend/.next/dev/server/middleware-build-manifest.js
+++ GuitarIO_Frontend/.next/dev/server/middleware-build-manifest.js
@@ -23,16 +23,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/login": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/login.js"
-    ],
-    "/play-song": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/play-song.js"
     ]
   }
 };
